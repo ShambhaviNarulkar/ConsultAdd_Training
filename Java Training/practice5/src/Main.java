9947054d2b7188a04f1404d2cee6d285f0db7609
Shambhavi Narulkar <101650207+ShambhaviNarulkar@users.noreply.github.com>
2023-02-08 17:40:04 +0530

    Add files via upload

---
+++ Java Training/practice5/src/Main.java
@@ -0,0 +1,20 @@
+class ExceptionPropagation{
+    void m(){
+        int data=10/0;
+    }
+    void n(){
+        m();
+    }
+    void p(){
+        try{
+            n();
+        }catch(Exception e){
+            System.out.println("exception handled");
+        }
+    }
+    public static void main(String args[]){
+        ExceptionPropagation obj=new ExceptionPropagation();
+        obj.p();
+        System.out.println("hi");
+    }
+}
